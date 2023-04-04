5a7a09c6a03b75a5042a1baf2e1ddd8b3c58d9e6
Tyan-0978 <960007nathan@gmail.com>
2023-04-05 01:31:36 +0800

    change coding style

--- eval_qtz.py
+++ eval_qtz.py
@@ -4,7 +4,9 @@
 
 import os
 import torch
+
 from utils import helper, datasets, alexnet_utils, vgg16_utils
+
 
 # settings -----------------------------------------------------------
 # cuda is not supported for quantization
@@ -19,14 +21,16 @@ print('Loading dataset ...')
 transform = alexnet_utils.use_alexnet_transform()
 #transform = vgg16_utils.use_vgg16_transform()
 
-_, test_loader = datasets.prepare_cifar10_loaders(transform)
+_, test_loader = datasets.prepare_cifar100_loaders(transform)
 
 print('Done')
 print('')
 
+
 # load models --------------------------------------------------------
-fp32_model_filename = 'alexnet_cifar10.pt'
-qtz_model_filename = 'alexnet_cifar10_qtz_histogram.pt'
+fp32_model_filename = 'alexnet_cifar100_acc67.pt'
+qtz_model_filename = 'alexnet_qat_cifar100_acc70.pt'
+#qtz_model_filename = 'vgg16_imagenet_qtz_histogram.pt'
 
 model_dir = './models/'
 fp32_model_filepath = os.path.join(model_dir, fp32_model_filename) 
@@ -35,6 +39,8 @@ qtz_model_filepath = os.path.join(model_dir, qtz_model_filename)
 print(f'Loading FP32 model from {fp32_model_filepath} ...', end=' ')
 
 fp32_model = helper.load_model(fp32_model_filepath)
+#fp32_model = alexnet_utils.create_alexnet_model()
+#fp32_model = vgg16_utils.create_vgg16_model()
 fp32_model.eval()
 
 print('Done')
@@ -47,28 +53,29 @@ qtz_model.eval()
 print('Done')
 print('')
 
-# test accuracy ------------------------------------------------------
-print('Evaluating quantized model ...')
 
+# test accuracy ------------------------------------------------------
 topk = (1, 5)
 
-_, int8_top1_acc, int8_top5_acc = helper.evaluate_model_topk(
-  model=qtz_model, 
-  test_loader=test_loader, 
-  device=qtz_device, 
-  topk=topk
-)
+with torch.no_grad():
+    print('Evaluating FP32 model ...')
+    _, fp32_top1_acc, fp32_top5_acc = helper.evaluate_model_topk(
+        model=fp32_model, 
+        test_loader=test_loader, 
+        device=device, 
+        topk=topk
+        )
+    print(f'FP32: {fp32_top1_acc:.5f} (top 1) / {fp32_top5_acc:.5f} (top 5)')
 
-print('Evaluating FP32 model ...')
-_, fp32_top1_acc, fp32_top5_acc = helper.evaluate_model_topk(
-  model=fp32_model, 
-  test_loader=test_loader, 
-  device=device, 
-  topk=topk
-)
+    print('Evaluating quantized model ...')
+    _, int8_top1_acc, int8_top5_acc = helper.evaluate_model_topk(
+        model=qtz_model, 
+        test_loader=test_loader, 
+        device=qtz_device, 
+        topk=topk
+        )
+    print(f'INT8: {int8_top1_acc:.5f} (top 1) / {int8_top5_acc:.5f} (top 5)')
 
-print(f'FP32: {fp32_top1_acc:.5f} (top 1) / {fp32_top5_acc:.5f} (top 5)')
-print(f'INT8: {int8_top1_acc:.5f} (top 1) / {int8_top5_acc:.5f} (top 5)')
 
 # measure latency ----------------------------------------------------
 print('Measuring inference latency ...')
@@ -77,18 +84,18 @@ inf_size = (10, 3, 256, 256)
 num_samples = 100
 
 fp32_inf_latency = helper.measure_inference_latency(
-  model=fp32_model, 
-  device=qtz_device, 
-  input_size=inf_size,
-  num_samples=num_samples
-)
+    model=fp32_model, 
+    device=qtz_device, 
+    input_size=inf_size,
+    num_samples=num_samples
+    )
 
 print(f'FP32 CPU inference latency: {(fp32_inf_latency * 1000):.3f} ms / sample')
 
 int8_inf_latency = helper.measure_inference_latency(
-  model=qtz_model, 
-  device=qtz_device, 
-  input_size=inf_size,
-  num_samples=num_samples
-)
+    model=qtz_model, 
+    device=qtz_device, 
+    input_size=inf_size,
+    num_samples=num_samples
+    )
 print(f'INT8 CPU inference latency: {(int8_inf_latency * 1000):.3f} ms / sample')
