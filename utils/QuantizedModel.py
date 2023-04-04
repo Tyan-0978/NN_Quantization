6d29ba7713decf75cb0569cd861711c0b4964e22
Tyan-0978 <960007nathan@gmail.com>
2023-04-05 01:16:20 +0800

    add method for saving quantization parameters

--- utils/QuantizedModel.py
+++ utils/QuantizedModel.py
@@ -2,27 +2,45 @@
 # Quantized AlexNet Module
 # ------------------------------------------------------------------------------
 
+import pickle
+
 import torch
 import torch.nn as nn
 
-class QuantizedModel(nn.Module):
-  def __init__(self, model_fp32):
-    super(QuantizedModel, self).__init__()
-    # QuantStub converts tensors from floating point to quantized.
-    # This will only be used for inputs.
-    self.quant = torch.quantization.QuantStub()
-    # DeQuantStub converts tensors from quantized to floating point.
-    # This will only be used for outputs.
-    self.dequant = torch.quantization.DeQuantStub()
-    # FP32 model
-    self.model_fp32 = model_fp32
 
-  def forward(self, x):
-    # manually specify where tensors will be converted from floating
-    # point to quantized in the quantized model
-    x = self.quant(x)
-    x = self.model_fp32(x)
-    # manually specify where tensors will be converted from quantized
-    # to floating point in the quantized model
-    x = self.dequant(x)
-    return x
+class QuantizedModel(nn.Module):
+    def __init__(self, model_fp32):
+        super(QuantizedModel, self).__init__()
+        self.quant = torch.quantization.QuantStub()
+        self.dequant = torch.quantization.DeQuantStub()
+        self.model_fp32 = model_fp32
+
+    def forward(self, x):
+        x = self.quant(x)
+        x = self.model_fp32(x)
+        x = self.dequant(x)
+        return x
+
+    def save_params(self, file_path):
+        params = {
+            'layer_indices': [],
+            'weights': [],
+            'biases': [],
+            'weight_scales': [],
+            'act_scales': [],
+        }
+
+        params['act_scales'].append(self.quant.scale.item())
+
+        for i, layer in enumerate(self.model_fp32.features):
+            if type(layer) == torch.ao.nn.quantized.modules.conv.Conv2d:
+                params['layer_indices'].append(i)
+                params['weights'].append(layer.weight().int_repr().tolist())
+                params['biases'].append(layer.bias().tolist())
+                params['weight_scales'].append(layer.weight().q_scale())
+                params['act_scales'].append(layer.scale)
+
+        params['act_scales'].pop()  # scale of last layer is not used
+
+        with open(file_path, 'wb') as file:
+            pickle.dump(params, file)
