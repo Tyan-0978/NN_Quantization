bebbb012c8bd87b60c0c514fe47b826573f8d959
Tyan-0978 <960007nathan@gmail.com>
2023-04-05 01:15:19 +0800

    save quantization parameters by pickle

--- alexnet_qtz.py
+++ alexnet_qtz.py
@@ -1,12 +1,15 @@
 # ------------------------------------------------------------------------------
-# Quantization on AlexNet for CIFAR10
+# AlexNet Quantization
 # ------------------------------------------------------------------------------
 
 import os
 import copy
+import pickle
+
 import torch
 from torch.quantization import QConfig, MinMaxObserver, HistogramObserver
 import torchvision
+
 from utils import helper, datasets, alexnet_utils
 from utils.QuantizedModel import QuantizedModel
 
@@ -19,13 +22,13 @@ device = torch.device("cpu") # CUDA is not supported for quantization
 print('Loading dataset ...')
 
 transform = alexnet_utils.use_alexnet_transform()
-train_batch_size = 4
+train_batch_size = 1
 eval_batch_size = 1
 
-train_loader, test_loader = datasets.prepare_cifar10_loaders(
-  data_transform=transform,
-  train_batch_size=train_batch_size,
-  eval_batch_size=eval_batch_size
+train_loader, test_loader = datasets.prepare_imagenet_loaders(
+    data_transform=transform,
+    train_batch_size=train_batch_size,
+    eval_batch_size=eval_batch_size,
 )
 
 print('Done')
@@ -33,48 +36,18 @@ print('')
 
 # load Alexnet model (pretrained by CIFAR10)
 model_dir = './models/'
-fp32_model_name = 'alexnet_cifar10.pt'
-fp32_model_path = os.path.join(model_dir, fp32_model_name)
+#fp32_model_name = 'alexnet_cifar10.pt'
+#fp32_model_path = os.path.join(model_dir, fp32_model_name)
+#print(f'Loading FP32 model from {fp32_model_path} ...')
 
-print(f'Loading FP32 model from {fp32_model_path} ...')
+#fp32_model = helper.load_model(fp32_model_path)
+fp32_model = alexnet_utils.create_alexnet_model()
 
-fp32_model = helper.load_model(fp32_model_path)
 fp32_model.to(device)
 print(fp32_model)
 
 print('Done')
 print('')
-
-'''
-# model fusion -------------------------------------------------------
-print('Fusing FP32 Model ...')
-fused_model = copy.deepcopy(fp32_model)
-
-fp32_model.eval()
-fused_model.eval()
-
-for module_name, module in fused_model.named_children():
-  if "layer" in module_name:
-    for basic_block_name, basic_block in module.named_children():
-      torch.quantization.fuse_modules(
-        basic_block, 
-	[["conv1", "bn1", "relu1"], ["conv2", "bn2"]], 
-	inplace=True
-      )
-      for sub_block_name, sub_block in basic_block.named_children():
-        if sub_block_name == "downsample":
-          torch.quantization.fuse_modules(sub_block, [["0", "1"]], inplace=True)
-
-print('Done')
-assert helper.model_equivalence(
-  model_1=fp32_model, 
-  model_2=fused_model, 
-  device=cpu_device, 
-  rtol=1e-03, atol=1e-06, num_tests=100, input_size=(1,3,64,64)
-), "Fused model is not equivalent to the original model!"
-print('Model equivalence test passed')
-print('')
-'''
 
 # quantization -------------------------------------------------------
 print('Start quantization ...')
@@ -86,12 +59,12 @@ default_quantization_config = torch.quantization.default_qconfig
 quantized_model.qconfig = default_quantization_config
 '''
 act_config = HistogramObserver.with_args(
-  dtype=torch.quint8,
-  qscheme=torch.per_tensor_symmetric,
+    dtype=torch.quint8,
+    qscheme=torch.per_tensor_symmetric,
 )
 weight_config = HistogramObserver.with_args(
-  dtype=torch.qint8,
-  qscheme=torch.per_tensor_symmetric,
+    dtype=torch.qint8,
+    qscheme=torch.per_tensor_symmetric,
 )
 
 quantization_config = QConfig(activation=act_config, weight=weight_config)
@@ -124,11 +97,23 @@ _, eval_accuracy = helper.evaluate_model_topk(
 
 print(f'Model accuracy: {eval_accuracy}\n')
 
-# save quantized model
+# save quantized model and quantization parameters
 save_model = False
-qtz_model_name = f'alexnet_cifar10_qtz_histogram.pt'
 
 if save_model:
-  qtz_model_path = os.path.join(model_dir, qtz_model_name)
-  helper.save_torchscript_model(quantized_model, qtz_model_path)
-  print(f'Quantization model saved to {qtz_model_path}')
+    prefix = f'alexnet_imagenet_qtz_acc{int(eval_accuracy*100)}'
+
+    params_dir = 'params/'
+    if not os.path.exists(params_dir):
+        os.makedirs(params_dir)
+    params_name = prefix + '_params.pkl'
+    params_path = os.path.join(params_dir, params_name)
+    quantized_model.save_params(params_path)
+    print(f'Quantization parameters saved to {params_path}')
+
+    qtz_model_name = prefix + '_model.pt'
+    qtz_model_path = os.path.join(model_dir, qtz_model_name)
+    helper.save_torchscript_model(quantized_model, qtz_model_path)
+    print(f'Quantization model saved to {qtz_model_path}')
+
+# end
