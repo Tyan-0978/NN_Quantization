ad1192a87740f0b97bb8a2f216dead297b56b7ea
Tyan-0978 <960007nathan@gmail.com>
2023-04-07 23:17:29 +0800

    write results to log file

--- psum_exp.py
+++ psum_exp.py
@@ -17,6 +17,10 @@ from utils import alexnet_utils
 from utils import psum
 
 
+if len(sys.argv) != 3:
+    print('Missing arguments; Please specify data range.')
+    sys.exit()
+
 # create models
 # original AlexNet model
 model = alexnet_utils.create_alexnet_model()
@@ -28,8 +32,10 @@ qtz_model_path = os.path.join(model_dir, qtz_model_name)
 qtz_model = helper.load_torchscript_model(qtz_model_path)
 
 # psum customized AlexNet model
-psum_bits = 32
+psum_bits = 20
 psum_num_ops = 36
+print(f'Psum bits: {psum_bits}')
+print(f'Number of operands: {psum_num_ops}')
 
 custom_model = psum.CustomAlexnet(
     ref_model=model,
@@ -49,24 +55,18 @@ custom_model.customize(params)
 
 
 # dataset
-dataset_size = 50000
-num_per_cat = 50
-data_offset = 0
-data_indices_1 = [i for i in range(data_offset, dataset_size, num_per_cat)]
-
-num_data = 10000
-data_offset = 40000
-data_indices_2 = [i + data_offset for i in range(num_data)]
+transform = alexnet_utils.use_alexnet_transform()
+test_set = datasets.ImageNet(root="./data", split='val', transform=transform)
+dataset_size = len(test_set)
+print(f'Size of dataset: {dataset_size}')
 
 start = int(sys.argv[1]) - 1
 end = int(sys.argv[2])
-data_indices_3 = list(range(start, end))
+data_indices = list(range(start, end))
 
-print(f'Data range: {data_indices_3[0]} - {data_indices_3[-1]}')
+print(f'Dataset index range: {data_indices[0]} - {data_indices[-1]}')
 
-transform = alexnet_utils.use_alexnet_transform()
-test_set = datasets.ImageNet(root="./data", split='val', transform=transform)
-test_subset = Subset(test_set, data_indices_3)
+test_subset = Subset(test_set, data_indices)
 test_loader = DataLoader(test_subset)
 
 
@@ -74,26 +74,45 @@ test_loader = DataLoader(test_subset)
 cpu_device = torch.device('cpu')
 gpu_device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+run_orig = False
+run_qtz = False
+run_custom = True
+
 topk = (1, 5)
 with torch.no_grad():
-    _, fp32_top1, fp32_top5 = helper.evaluate_model_topk(
-        model, test_loader,
-        device=gpu_device,
-        topk=topk,
-        )
-    print(f'Original model accuracy: {fp32_top1:.5f} (top 1) / {fp32_top5:.5f} (top 5)')
+    if run_orig:
+        _, fp32_top1, fp32_top5 = helper.evaluate_model_topk(
+            model, test_loader,
+            device=gpu_device,
+            topk=topk,
+            )
+        print('Original model accuracy: ', end='')
+        print(f'{fp32_top1:.5f} (top 1) / {fp32_top5:.5f} (top 5)')
 
-    _, qtz_top1, qtz_top5 = helper.evaluate_model_topk(
-        qtz_model, test_loader,
-        device=cpu_device,
-        topk=topk,
-        )
-    print(f'Quantized model accuracy: {qtz_top1:.5f} (top 1) / {qtz_top5:.5f} (top 5)')
+    if run_qtz:
+        _, qtz_top1, qtz_top5 = helper.evaluate_model_topk(
+            qtz_model, test_loader,
+            device=cpu_device,
+            topk=topk,
+            )
+        print('Quantized model accuracy: ', end='')
+        print(f'{qtz_top1:.5f} (top 1) / {qtz_top5:.5f} (top 5)')
 
-    _, custom_top1, custom_top5 = helper.evaluate_model_topk(
-        custom_model, test_loader,
-        device=cpu_device,
-        topk=topk,
-        )
-    print(f'Custom model accuracy: {custom_top1:.5f} (top 1) / {custom_top5:.5f} (top 5)')
+    if run_custom:
+        _, custom_top1, custom_top5 = helper.evaluate_model_topk(
+            custom_model, test_loader,
+            device=cpu_device,
+            topk=topk,
+            )
+        print('Custom model accuracy: ', end='')
+        print(f'{custom_top1:.5f} (top 1) / {custom_top5:.5f} (top 5)')
 
+log_name = 'acc_log.txt'
+with open(log_name, 'a') as log_file:
+    log_file.write(f'Psum bits: {psum_bits:2}  /  ')
+    log_file.write(f'Number of operands: {psum_num_ops:3}  /  ')
+    log_file.write(f'Data range: {start} - {end}\n')
+    log_file.write(f'Accuracy: ')
+    log_file.write(f'{custom_top1:.5f} (top 1) / {custom_top5:.5f} (top 5)\n')
+    log_file.write('-' * 75 + '\n')
+    print(f'Record written in {log_name}')
